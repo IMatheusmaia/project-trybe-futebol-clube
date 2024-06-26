c530626fcfe8dd19fc84b2c15f8e5a02f1684bb3
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-06-26 00:52:39 -0300

    requisito 24

--- app/backend/src/service/leaderboard.service.ts
+++ app/backend/src/service/leaderboard.service.ts
@@ -39,7 +39,7 @@ const calcGoalsFavor = (games: any) => games
 const calcGoalsOwn = (games: any) => games
   .reduce((acc: any, curr: any) => acc + curr.awayTeamGoals, 0);
 
-const generateLeaderBoard = (data: any[], status: string) => data.map((games: any) => {
+const primaryAttributes = (games: any, status: string) => {
   const name = games[0][`${status}Team`].teamName;
   const totalGames = games.length;
   const totalVictories = calcTotalVictories(games);
@@ -47,24 +47,51 @@ const generateLeaderBoard = (data: any[], status: string) => data.map((games: an
   const totalLosses = calcTotalLosses(games);
   const goalsFavor = calcGoalsFavor(games);
   const goalsOwn = calcGoalsOwn(games);
-  const totalPoints = (totalVictories * 3) + (totalDraws * 1);
-  return { name,
-    totalPoints,
+  return {
+    name,
     totalGames,
     totalVictories,
     totalDraws,
     totalLosses,
     goalsFavor,
-    goalsOwn };
-});
+    goalsOwn,
+  };
+};
+const secondaryAttributes = (games: any, status: string) => {
+  const { totalVictories, totalDraws,
+    goalsFavor, totalGames, goalsOwn } = primaryAttributes(games, status);
+
+  const totalPoints = (totalVictories * 3) + (totalDraws * 1);
+  const goalsBalance = goalsFavor - goalsOwn;
+  const efficiency = Number(((totalPoints / (totalGames * 3)) * 100).toFixed(2));
+  return {
+    totalPoints,
+    efficiency,
+    goalsBalance,
+  };
+};
+const generateLeaderBoard = (data: any[], status: string) => data.map((games: any) => ({
+  ...primaryAttributes(games, status),
+  ...secondaryAttributes(games, status),
+}));
+
 const listLeaderTeams = async (status: 'home' | 'away' = 'home') => {
   const { data } = await matchService.getAllInProgress(false);
 
   const listByName = listGamesByName(data, status);
   const leaderBoard = generateLeaderBoard(listByName, status);
+  const orderedLeaderBoard = leaderBoard.sort((a, b) => {
+    if (a.totalPoints !== b.totalPoints) {
+      return b.totalPoints - a.totalPoints;
+    }
+    if (a.goalsBalance !== b.goalsBalance) {
+      return b.goalsBalance - a.goalsBalance;
+    }
+    return b.goalsFavor - a.goalsFavor;
+  });
 
   if (leaderBoard) {
-    return { status: 'SUCCESSFUL', data: leaderBoard };
+    return { status: 'SUCCESSFUL', data: orderedLeaderBoard };
   }
   return { status: 'CONFLICT', data: [] };
 };
